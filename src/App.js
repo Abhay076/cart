533190b4b4a31acdcb67023daacaef2a70f47d2f
Abhay 076 <yadavabhaycs@gmail.com>
2021-10-14 08:33:42 +0530

    Reading Data from firebase -1 in Changes App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-import React from 'react';
-import Cart from './Cart';
-import Navbar from './Navbar';
+import React from "react";
+// import "./App.css";
+// import CartItem from './CartItem';
+import Cart from "./Cart";
+import Navbar from "./Navbar";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 class App extends React.Component {
-
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1539874754764-5a96559165b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1230&q=80',
-          id: 1
-        },
-        {
-          price: 999,
-          title: 'Mobile Phone',
-          qty: 10,
-          img: 'https://images.unsplash.com/photo-1605236453806-6ff36851218e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=464&q=80',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 4,
-          img: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1964&q=80',
-          id: 3
-        }
-      ]
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
+      products: [],
+      loading: true
+    };
   }
-  handleIncreaseQuantity = (product) => {
-    console.log('Heyy please inc the qty of ', product);
+
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection("products")
+      .get()
+      .then(snapshot => {
+        const products = snapshot.docs.map(doc => {
+          const data = doc.data();
+          data["id"] = doc.id;
+          return data;
+        });
+        this.setState({ products: products, loading: false });
+      });
+  }
+
+  handleIncreaseQuantity = product => {
     const { products } = this.state;
     const index = products.indexOf(product);
 
@@ -43,68 +38,73 @@ class App extends React.Component {
 
     this.setState({
       products
-    })
-  }
-  handleDecreaseQuantity = (product) => {
-    console.log('Heyy please inc the qty of ', product);
+    });
+  };
+
+  handleDecreaseQuantity = product => {
     const { products } = this.state;
     const index = products.indexOf(product);
 
     if (products[index].qty === 0) {
       return;
     }
-
     products[index].qty -= 1;
 
     this.setState({
       products
-    })
-  }
-  handleDeleteProduct = (id) => {
+    });
+  };
+
+  handleDeleteProduct = id => {
     const { products } = this.state;
 
-    const items = products.filter((item) => item.id !== id); // [{}]
+    const items = products.filter(product => product.id !== id);
 
     this.setState({
       products: items
-    })
-  }
+    });
+  };
 
-  getCartCount = () => {
+  getcountOfCartItems = () => {
     const { products } = this.state;
-
     let count = 0;
 
-    products.forEach((product) => {
+    products.forEach(product => {
       count += product.qty;
-    })
+    });
 
     return count;
-  }
+  };
 
-  getCartTotal = () => {
+  getcartTotal = () => {
     const { products } = this.state;
-
     let cartTotal = 0;
 
-    products.map((product) => {
-      cartTotal = cartTotal + product.qty * product.price
-    })
+    products.map(product => {
+      if (product.qty > 0) {
+        cartTotal = cartTotal + product.qty * product.price;
+      }
+      return "";
+    });
 
     return cartTotal;
-  }
-  render () {
-    const { products } = this.state;
+  };
+
+  render() {
+    const { products, loading } = this.state;
     return (
       <div className="App">
-        <Navbar count={this.getCartCount()} />
+        <Navbar count={this.getcountOfCartItems()} />
         <Cart
-          products={products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
+          products={products}
         />
-        <div style={ {padding: 10, fontSize: 20} }>TOTAL: {this.getCartTotal()} </div>
+        {loading && <h1>Loading Products...</h1>}
+        <div style={{ padding: 10, fontSize: 20 }}>
+          TOTAL : {this.getcartTotal()}
+        </div>
       </div>
     );
   }
